28747a731d565061f0790e1fd1580abdf50876ff
Dwayne <dwayneletran17@gmail.com>
2026-04-26 00:25:40 +0800

    Branding and Icon Update: Use rounded SVG for PWA and Meta icons

--- app/app.vue
+++ app/app.vue
@@ -39,7 +39,7 @@ useHead({
   link: [
     { rel: 'manifest', href: '/manifest.json?v=15' },
     { rel: 'icon', type: 'image/svg+xml', href: '/favicon-rounded.svg?v=15' },
-    { rel: 'apple-touch-icon', href: '/icons/apple-touch-icon.png?v=15' }
+    { rel: 'apple-touch-icon', href: '/favicon-rounded.svg' }
   ]
 });
 </script>
